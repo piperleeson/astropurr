2625654af1565310566adecb8f708d240cd116f0
Piper Leeson <leesonpip@gmail.com>
2025-01-05 23:11:08 -0600

    login page added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('logInButton').addEventListener('click', function() {
+    window.location.href = 'login.html'; 
+});
+document.getElementsByClassName('chartGenButton').addEventListener('click', function() {
+    window.location.href = 'chart-generator.html'; 
+});
