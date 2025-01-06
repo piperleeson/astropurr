6d8aa794df3c8c454bfa18adf0839be0b683881b
Piper Leeson <leesonpip@gmail.com>
2025-01-05 23:21:37 -0600

    once again pages update

---
+++ dist/bundle.js
@@ -0,0 +1,1 @@
+document.getElementsByClassName("logInButton").addEventListener("click",(function(){window.location.href="login.html"})),document.getElementsByClassName("chartGenButton").addEventListener("click",(function(){window.location.href="chart-generator.html"}));
