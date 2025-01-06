ed9c532f4bc58a1d0e961fdbc1bf564e30ea7f9a
Piper Leeson <leesonpip@gmail.com>
2025-01-05 21:48:18 -0600

    webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',  // Replace with your entry file
@@ -16,10 +17,20 @@ module.exports = {
         test: /\.js$/,  // Match JavaScript files
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',  // Use Babel to transpile JS if needed
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],  // Use Babel to transpile JS if needed
+            },
         },
-      },
+      }
       // Add other rules if necessary (for styles, images, etc.)
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your source HTML file
+      filename: 'index.html',
+        }
+    )
+    ]
 };
